7c2b670ca0622e3426741221d44272e2d3ae68d2
louka <loukarmonica@gmail.com>
2024-12-29 19:58:13 +0100

    attempt to add color changing on Gem

--- src/main/java/fr/skylined/gemmology/GemmologyClient.java
+++ src/main/java/fr/skylined/gemmology/GemmologyClient.java
@@ -1,10 +1,21 @@
 package fr.skylined.gemmology;
 
+import fr.skylined.gemmology.component.ModComponents;
+import fr.skylined.gemmology.item.ModItems;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.client.color.item.ItemColorProvider;
+import net.minecraft.item.ItemStack;
 
 public class GemmologyClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        ColorProviderRegistry.ITEM.register(new ItemColorProvider() {
+            @Override
+            public int getColor(ItemStack stack, int layer)
+            {
+                return layer == 0 ? (int) stack.getOrDefault(ModComponents.WAVE_LENGTH, 380).floatValue() : 0xFFFFFF;
+            }
+        }, ModItems.GEM);
     }
 }
